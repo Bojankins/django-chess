2744864455f99df2711b765d093a4225c5f304ed
Bojankins <matthew.olson808@gmail.com>
2020-05-24 23:55:12 -0500

    added static js and chessgame route

--- chessgame/views.py
+++ chessgame/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index( request ):
-    return HttpResponse(" Chess Game! ")
+    return render(request, 'chessgame/index.html')
