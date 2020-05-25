5d94b8d8287915602472f25b22c83c94ab37228b
Bojankins <matthew.olson808@gmail.com>
2020-05-25 00:17:59 -0500

    made generate board function

--- chessgame/static/js/main.js
+++ chessgame/static/js/main.js
@@ -1,3 +1,16 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    
+    generateBoard();
+
+    function generateBoard() {
+        for(i = 1; i <= 8; i ++){
+            $('#board').append('<tr id='+i+'></tr>');
+
+            for(j = 1; j <= 8; j ++){
+                $('#'+i).append('<td class='+j+'></td>')
+            }
+        };
+
+      };
+
 });
